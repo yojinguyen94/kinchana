b1152f370795c099d30bad60c78e37340561a901
Yoji Nguyen <yoji.nguyen9414@gmail.com>
2025-06-22 02:17:46 +0700

    Update or-trak-hu.sh

--- or-trak-hu.sh
+++ or-trak-hu.sh
@@ -197,7 +197,7 @@ fi
 echo $currentblock > lastBlock.txt
 
 echo -e "${GREEN}Current Block: $currentblock${NC}"
-block=$((currentblock - 25))
+block=$((currentblock - 20))
 totalThreads=$(docker ps | grep $imageName | wc -l)
 oldTotalThreads=$totalThreads
 setNewThreadUAM=0

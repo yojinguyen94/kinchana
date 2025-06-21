42b5d09858e4d16f6bdda406cd3c7bf5d93406eb
Yoji Nguyen <yoji.nguyen9414@gmail.com>
2025-06-21 15:22:22 +0700

    Update or-trak-hu.sh

--- or-trak-hu.sh
+++ or-trak-hu.sh
@@ -117,9 +117,11 @@ minute=$(date +%M)
 
 if [[ "$minute" == "00" && ( "$hour" == "04" || "$hour" == "08" || "$hour" == "12" || "$hour" == "16" || "$hour" == "00" ) ]]; then
     if [[ $cpu_cores -eq 4 ]]; then
-        echo "Restarting traffmonetizer..."
+        echo "Restarting traffmonetizer & earnfm ..."
         docker restart $(docker ps -aq -f "ancestor=traffmonetizer/cli_v2")
+        docker restart $(docker ps -aq -f "ancestor=earnfm/earnfm-client:latest")
         docker run -it -d --name traffmonetizer --restart always --memory=100mb traffmonetizer/cli_v2 start accept --token ZDlwgs1MNS7yUh2o2Bv7VeLJCAebJvUiicrxAnH1jXI=
+        docker run -d --name earnfm --restart=always --memory=100mb -e EARNFM_TOKEN="4d45663a-5f9a-46ff-9efe-390cc4b9f3cc" earnfm/earnfm-client:latest
     fi
 fi
 

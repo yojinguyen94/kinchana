e964141c1e55d33e7e59bb4526ae2a6f52b84317
Yoji Nguyen <yoji.nguyen9414@gmail.com>
2025-06-25 21:39:26 +0700

    Update run_random_time.sh

--- run_random_time.sh
+++ run_random_time.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Delay random from 0 to 720 minutes (12 hours)
 DELAY_MINUTES=$(( RANDOM % 720 ))
 echo "⏳ Sleeping for $DELAY_MINUTES minutes before running script..."
 sleep "${DELAY_MINUTES}m"

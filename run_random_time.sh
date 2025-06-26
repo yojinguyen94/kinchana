0f351ea20b5e0930bd4b594d91fb7ddf35ff6114
Yoji Nguyen <yoji.nguyen9414@gmail.com>
2025-06-26 11:58:37 +0700

    Update run_random_time.sh

--- run_random_time.sh
+++ run_random_time.sh
@@ -5,4 +5,4 @@ DELAY_MINUTES=$(( RANDOM % 720 ))
 echo "⏳ Sleeping for $DELAY_MINUTES minutes before running script..."
 sleep "${DELAY_MINUTES}m"
 
-bash ./simulate_oci_user.sh
+bash /home/ubuntu/simulate_oci_user.sh

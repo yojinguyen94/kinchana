719ee91f13678aec1f18a63c84a1dc36a56b6cf9
Yoji Nguyen <yoji.nguyen9414@gmail.com>
2025-07-04 21:59:39 +0700

    Update simulate_oci_user.sh

--- simulate_oci_user.sh
+++ simulate_oci_user.sh
@@ -676,7 +676,7 @@ job7_create_volume() {
       
       log_action "$TIMESTAMP" "volume-create" "📦 Available Block Volume Storage: ${AVAILABLE_STORAGE} GB" "info"
 
-      if [[ "$AVAILABLE_STORAGE" -lt 50 ]]; then
+      if [[ "$AVAILABLE_STORAGE" -lt 1000 ]]; then
 	  log_action "$TIMESTAMP" "volume-create" "⚠️ Skipped: only $AVAILABLE_STORAGE GB left" "skipped"
 	  return
       fi

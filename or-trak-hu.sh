de6b922c1bd689f24884c3a31c706735b9223cd5
Yoji Nguyen <yoji.nguyen9414@gmail.com>
2026-01-25 13:24:51 +0700

    Update or-trak-hu.sh

--- or-trak-hu.sh
+++ or-trak-hu.sh
@@ -269,27 +269,27 @@ echo "Total Threads: $totalThreads"
 #    setNewThreadUAM=1
 #fi
 
-if [[ $cpu_cores -eq 48 && $totalThreads -lt 10 ]]; then
-#    totalThreads=14
-#    setNewThreadUAM=1
+if [[ $cpu_cores -eq 48 && $totalThreads -lt 14 ]]; then
+    totalThreads=14
+    setNewThreadUAM=1
     setNewThreadUAMNotify=1
 fi
 
 if [[ $cpu_cores -eq 256 && $totalThreads -lt 50 ]]; then
-#    totalThreads=55
+#    totalThreads=50
 #    setNewThreadUAM=1
     setNewThreadUAMNotify=1
 fi
 
-if [[ $cpu_cores -eq 48 && $totalThreads -gt 10 ]]; then
-    docker rm -f uam_13 uam_14 uam_12 uam_11
-    totalThreads=10
-fi
+#if [[ $cpu_cores -eq 48 && $totalThreads -gt 10 ]]; then
+#    docker rm -f uam_13 uam_14 uam_12 uam_11
+#    totalThreads=10
+#fi
 
-if [[ $cpu_cores -eq 256 && $totalThreads -gt 50 ]]; then
-    docker rm -f uam_55 uam_54 uam_53 uam_52 uam_51
-    totalThreads=50
-fi
+#if [[ $cpu_cores -eq 256 && $totalThreads -gt 50 ]]; then
+#    docker rm -f uam_55 uam_54 uam_53 uam_52 uam_51
+#    totalThreads=50
+#fi
 
 if [ "$setNewThreadUAMNotify" -gt 0 ]; then
     echo -e "${YELLOW}LOW THREAD UAM WARNING!!!${NC}"

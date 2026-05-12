b71e77d8412d11ba40a9f912a8e932dcbff553a8
Yoji Nguyen <yoji.nguyen9414@gmail.com>
2026-05-12 22:42:13 +0700

    Update pre-install-u.sh

--- pre-install-u.sh
+++ pre-install-u.sh
@@ -16,7 +16,7 @@ else
     echo "First network interface: $net"
 fi
 
-sudo iptables -t nat -I POSTROUTING -s 172.17.0.1 -j SNAT --to-source $(ip addr show $net | grep "inet " | grep -v 127.0.0.1|awk 'match($0, /(10.[0-9]+\.[0-9]+\.[0-9]+)/) {print substr($0,RSTART,RLENGTH)}')
+sudo iptables -t nat -I POSTROUTING -s 172.17.0.1 -j SNAT --to-source $(ip addr show $net | grep "inet " | grep -v 127.0.0.1 | awk 'match($0, /(10\.[0-9]+\.[0-9]+\.[0-9]+)/) {print substr($0,RSTART,RLENGTH)}' | head -n1)
 sudo apt purge ntp -y
 sudo systemctl start systemd-timesyncd
 sudo systemctl status systemd-timesyncd >null

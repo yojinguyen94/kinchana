6e7d42cdf4e61e0e765b1aa35b2dec6cb5834e0f
Yoji Nguyen <yoji.nguyen9414@gmail.com>
2026-07-09 13:12:01 +0700

    Update and rename pre-install-u.sh to pre-install-u-24.sh

--- pre-install-u-24.sh
+++ pre-install-u-24.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+export NEEDRESTART_MODE=l
+export DEBIAN_FRONTEND=noninteractive
 sudo apt update
-sudo apt install nload && sudo apt install docker.io -y && sudo sed -ie 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1'/g /etc/sysctl.conf && sudo sysctl --system && sudo apt install docker-compose -y
+if ! command -v docker >/dev/null 2>&1; then
+    sudo apt-get install -y docker.io
+fi
+sudo apt install nload && sudo sed -ie 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1'/g /etc/sysctl.conf && sudo sysctl --system && sudo apt install docker-compose -y
 sudo chmod 666 /var/run/docker.sock
 sudo iptables -F
 sudo iptables -A INPUT -p all -j ACCEPT
